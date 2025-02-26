84a879262ceecfb4d591e59e94de678dfab3d038
yminjuu <ymj0580@naver.com>
2025-02-26 22:37:48 +0900

    feat: step 3 기본 골격 및 퍼블리싱

---
+++ src/components/UI/Step3_Button.tsx
@@ -0,0 +1,24 @@
+interface ButtonProps {
+  id: number;
+  text: string;
+  isSelected: boolean;
+  onSelection: Function;
+}
+
+const Step3_Button = ({ id, text, isSelected, onSelection }: ButtonProps) => {
+  return (
+    <div
+      className={`h-11 px-[72px] py-2.5 rounded-[32px] border-2 justify-center items-center gap-2.5 inline-flex cursor-pointer
+  ${isSelected ? `border-[#ff4f4f] bg-[#ffecec]` : `border-[#9a9a9a]`}`}
+      onClick={() => onSelection(id)}
+    >
+      <div
+        className={`text-center text-xl font-normal font-['Inter'] ${isSelected ? `text-[#ff0e0e]` : `text-[#000000]`}`}
+      >
+        {text}
+      </div>
+    </div>
+  );
+};
+
+export default Step3_Button;
--- src/components/games/FindingRabbit.tsx
+++ src/components/games/FindingRabbit.tsx
@@ -1,5 +1,58 @@
+import { useState } from "react";
+import Step3_Button from "../UI/Step3_Button";
+import { selectionData } from "../../constants/selectionData";
+
 function FindingRabbit() {
-  return <div>FindingRabbit</div>
+  const [selected, setSelected] = useState(0);
+
+  // 시작하기 모달 -> "Start"
+  // 모달 닫힘 -> "Close"
+  // 완료 모달 -> "Complete"
+  // 실패 모달 -> "Fail"
+  const [isModalOpen, setIsModalOpen] = useState("Start");
+
+  const onSelection = (index: number) => {
+    setSelected(index);
+    console.log(selected);
+
+    if (index === 3) alert("정답입니다!");
+    else alert("오답입니다!");
+  };
+
+  return (
+    <>
+      {/* {modalState} */}
+      <div className="w-full flex flex-col justify-center items-center">
+        <div className="w-full flex flex-col justify-center items-center relative">
+          <div className="text-center text-[#383838] text-base font-bold font-['Inter']">
+            STEP 03
+          </div>
+          <div className=" text-center">
+            <span className="text-[#852e2f] text-base font-bold font-['Inter']">
+              트렌디하고 섬세한 디자이너,
+              <br />
+            </span>
+            <span className="text-[#852e2f] text-xl font-bold font-['Inter']">
+              미어캣 구하기
+            </span>
+          </div>
+          <div className="text-center text-[#3e3e3e] text-sm font-semibold font-['Inter'] mt-10">
+            10초 안에 ‘멋쟁이사자처럼'을 찾아주세요!
+          </div>
+        </div>
+        <div className="w-full flex-col justify-center items-center gap-[17px] inline-flex mt-15">
+          {selectionData.map(({ id, text }) => (
+            <Step3_Button
+              id={id}
+              text={text}
+              isSelected={id === selected}
+              onSelection={onSelection}
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default FindingRabbit;
